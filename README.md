989c90e0d2ea55c2d7d828e28aae66c40b8c60bf
abdullah samman <abdullah.samman@mokus.io>
2017-02-24 11:52:27 +0200

    add .gitignore; add wox plugin files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Logtively-Wox
+## Installation:
+* clone the repo `git clone https://github.com/lobolabs/logtivly-wox.git`
+* copy your clone to `<WoxDirectory>\Plugins\<YourPluginDirectory>`, i.e.: C:\Users\evex\AppData\Local\Wox\app-1.3.183\Plugins
+* restart Wox
+
+## Usage:
+To use Logitvely on Wox, start wox and type `log`
---
+++ credentials/credentials.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+import httplib2
+import os
+import sys
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+CREDS_FILENAME = 'sheets.googleapis.com-python-quickstart.json'
+SPREADSHEET_ID = "17MlCBBriFSKC3gfWEIK0_6ENfLiIIB8E21hoDJRrPO8"
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('.')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   CREDS_FILENAME)
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+    return credentials
+
+def get_service_and_spreadsheetId():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = SPREADSHEET_ID
+    sys.stderr.write("\nspreadsheetId %s\n" % (spreadsheetId))
+
+    return service, spreadsheetId
+
+def main():
+    """Shows basic usage of the Sheets API.
+
+    Creates a Sheets API service object and prints the names and majors of
+    students in a sample spreadsheet:
+    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+
+if __name__ == '__main__':
+    main()
